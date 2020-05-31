fd6123130f1c110fb0d10fe285b996367314dd52
Crowdbotics <team@crowdbotics.com>
2020-05-31 18:28:16 +0000

    Update models

--- backend/clestia_school_17623/wsgi.py
+++ backend/clestia_school_17623/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clestia_school_17623.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clestia_school_17623.settings")
 
 application = get_wsgi_application()
